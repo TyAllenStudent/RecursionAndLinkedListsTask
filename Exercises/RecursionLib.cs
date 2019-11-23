f20fdfd9827438f2c60e32f4d46cdb9fb6d2ca4d
TY ALLEN <102058205@student.swin.edu.au>
2019-11-23 20:39:13 +1100

    added files

---
+++ Exercises/RecursionLib.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercises
+{
+    public class RecursionLib {
+
+        public int FactorialRec(int num, int ans = 1) {
+
+            if (num == 0) {
+                return ans;
+            }
+
+            return FactorialRec(num-1, ans*num);
+            // return num*FactorialRec(num-1)
+        }
+
+        /// <summary>
+        /// Creates a list of numbers from 1 to num.  Initialise numsList with empty List.
+        /// </summary>
+        /// <param name="num"></param>
+        /// <param name="numsList"></param>
+        /// <returns></returns>
+        public List<int> CountRec(int num, List<int> numsList) {
+            return new List<int>();
+        }
+
+        /// <summary>
+        /// Returns the result of num to the power of pow.
+        /// </summary>
+        /// <param name="num"></param>
+        /// <param name="pow"></param>
+        /// <param name="ans"></param>
+        /// <returns></returns>
+        public long ExponentialRec(int num, int pow, long ans = 1) {
+            return 1;
+        }
+
+        /// <summary>
+        /// Returns word (string) in reverse order.  Treats all characters the same.
+        /// </summary>
+        /// <param name="word"></param>
+        /// <param name="revWord"></param>
+        /// <returns></returns>    
+        public string WordReverseRec(string word, string revWord = "") {
+            return "";
+        }
+
+
+
+    }
+}
